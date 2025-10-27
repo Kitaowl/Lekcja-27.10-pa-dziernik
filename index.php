f46212ca0e2906b8a49b396ec327456e709d78d2
Kitaowl <mykyta646@gmail.com>
2025-10-27 13:39:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crude</title>
+</head>
+<body>
+    <p>ehrj</p>
+    <?php
+
+    $mySQLconnect = new mysqli("localhost", "root", "", "crud");
+
+    if ($mySQLconnect->connect_error) {
+        echo "Wystąpił błąd i nie udało się połączyć z bazą danych: ". $mySQLconnect->connect_error;
+    } else {
+        echo "Połączono z bazą danych! </br>";
+
+        $sql = 'SELECT * FROM crude';
+        $result = $mySQLconnect->query($sql);
+        // $result = $result->fetch_assoc();
+
+        while($row = $result->fetch_assoc()){
+            echo 'test'.$row['name'].' '.$row['lastname'];
+        }
+    }
+
+    ?>
+</body>
+</html>
